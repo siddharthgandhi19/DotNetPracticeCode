ac790022ded934305bab54e92d07bddafd9b0812
siddharthgandhi19 <siddharthgandhi.19@gmail.com>
2025-04-24 12:10:22 +0530

    dot net project setup

---
+++ DotNetPracticeCode/Program.cs
@@ -0,0 +1,14 @@
+﻿using DotNetPracticeCode.W3School;
+using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Basic basic = new Basic();
+            basic.PrintMessage();
+        }
+    }
+}
---
+++ DotNetPracticeCode/W3School/Basic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNetPracticeCode.W3School
+{
+    public class Basic
+    {
+        public void PrintMessage()
+        {
+            Console.WriteLine("Hello From Basic!");
+        }
+
+    }
+}
